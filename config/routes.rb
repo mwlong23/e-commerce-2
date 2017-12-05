3febfffe0a2b5ba58875abb96bc2f1f7f0cdd4d9
Mitchell <meechllada@gmail.com>
2017-12-05 11:30:47 -0800

    generates scaffolds for products, accounts orders and order_items

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :products
+  resources :orders
+  resources :order_items
+  resources :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
